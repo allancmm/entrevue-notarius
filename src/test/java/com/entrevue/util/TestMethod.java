e48ef3927b98e5bf0cacf859cf4f4fd8e4addf9b
Allan Martins <allanchristian22@gmail.com>
2022-04-23 10:58:22 -0400

    add TestMethod to help testing private methods

---
+++ src/test/java/com/entrevue/util/TestMethod.java
@@ -0,0 +1,22 @@
+package com.entrevue.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public interface TestMethod<D,A> {
+    void invoke(D d, A a) throws Throwable;
+
+    static <D,A> TestMethod<D,A> method(
+            Class<D> declarer, String name, Class<A> argType) throws ReflectiveOperationException {
+
+        Method method = declarer.getDeclaredMethod(name, argType);
+        method.setAccessible(true);
+        return (d, a) -> {
+            try {
+                method.invoke(d, a);
+            } catch(InvocationTargetException ex) {
+                throw ex.getTargetException();
+            }
+        };
+    }
+}
