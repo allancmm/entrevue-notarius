9d835c80e0876df1890f29b3f11b6e4c3a9c5998
Allan Martins <allanchristian22@gmail.com>
2022-04-23 11:06:30 -0400

    code style + more unit tests

--- src/test/java/com/entrevue/service/UrlServiceTest.java
+++ src/test/java/com/entrevue/service/UrlServiceTest.java
@@ -1,18 +1,27 @@
 package com.entrevue.service;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 
-import com.entrevue.dto.response.UrlGetResponse;
-import com.entrevue.dto.response.UrlPostResponse;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+
 import com.entrevue.model.Url;
+import com.entrevue.util.TestMethod;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -21,20 +30,17 @@ import com.entrevue.exception.ApiNoSuchAlgorithmException;
 import com.entrevue.exception.UrlShortenedNotFoundException;
 import com.entrevue.mapper.UrlMapper;
 import com.entrevue.repository.UrlRepository;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import com.entrevue.dto.response.UrlGetResponse;
+import com.entrevue.dto.response.UrlPostResponse;
 
 @ExtendWith(MockitoExtension.class)
 public class UrlServiceTest {
 
 	@Mock 
 	private UrlRepository urlRepository;
-	
+
+	private AutoCloseable closeable;
+
 	@Mock
 	private UrlMapper urlMapper;
 	
@@ -44,13 +50,20 @@ public class UrlServiceTest {
 	private final String urlShortened = "11XEGvA98x";
 	@BeforeEach
 	void setUp() {
-		MockitoAnnotations.openMocks(this);
+		closeable = MockitoAnnotations.openMocks(this);
 		underTest = new UrlService(urlRepository, urlMapper);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		closeable.close();
 	}
 
 	@Test
 	void itShouldThrowUrlShortenedNotFound() {
 		// given
+
+		// when
 
 		// then
 		assertThatThrownBy(() -> underTest.getByUrlShortened(urlShortened))
@@ -66,8 +79,8 @@ public class UrlServiceTest {
 		UrlGetResponse urlGetResponse = new UrlGetResponse(urlToShort, urlShortened);
 
 		// when
-		Mockito.when(urlRepository.findByUrlShortened(urlShortened)).thenReturn(Optional.of(urlOptional));
-		Mockito.when(urlMapper.mapToUrlGetResponse(urlOptional)).thenReturn(urlGetResponse);
+		when(urlRepository.findByUrlShortened(urlShortened)).thenReturn(Optional.of(urlOptional));
+		when(urlMapper.mapToUrlGetResponse(urlOptional)).thenReturn(urlGetResponse);
 
 		// then
 		assertThat(underTest.getByUrlShortened(urlShortened)).isEqualTo(urlGetResponse);
@@ -81,14 +94,15 @@ public class UrlServiceTest {
 		UrlPostResponse urlPostResponse = new UrlPostResponse();
 
 		// when 
+		when(urlMapper.mapToUrlPostResponse(urlToSave)).thenReturn(urlPostResponse);
+		when(urlMapper.mapToUrl(urlPostRequest, urlShortened)).thenReturn(urlToSave);
+		when(urlRepository.save(urlToSave)).thenReturn(urlToSave);
+
 		underTest.saveUrl(urlPostRequest);
-		Mockito.when(urlMapper.mapToUrlPostResponse(urlToSave)).thenReturn(urlPostResponse);
 
 		// then
 		verify(urlRepository).save(urlToSave);
 
-//		Url capturedUrl = urlArgumentCaptor.getValue();
-//		assertThat(capturedUrl).isEqualTo(urlToSave);
 	}
 	
 	@Test
@@ -98,24 +112,40 @@ public class UrlServiceTest {
 
 		// when
 		underTest.saveUrl(urlToSave);
-		
-		// then
 		ArgumentCaptor<Url> urlArgumentCaptor = ArgumentCaptor.forClass(Url.class);
 		verify(urlRepository).save(urlArgumentCaptor.capture());
-
 		Url capturedUrl = urlArgumentCaptor.getValue();
 
+		// then
 		assertThat(capturedUrl).isEqualTo(urlToSave);
 	}
 
-//	@Test
-//	void itShouldThrowApiNoSuchAlgorithmException() throws NoSuchAlgorithmException {
-//		// given
-//		Url urlToSave = new Url(urlShortened, urlToShort);
-//
-//		// when
-//		underTest.saveUrl(urlToSave);
-//		//
-//
-//	}
+	@Test
+	void itShouldNotThrowApiNoSuchAlgorithmException() throws NoSuchMethodException {
+		// given
+		UrlService urlService = new UrlService(urlRepository, urlMapper);
+
+		// when
+		Method privateMethod = UrlService.class.getDeclaredMethod("getMessageDigest", String.class );
+		privateMethod.setAccessible(true);
+
+		// then
+		assertDoesNotThrow(() -> {
+			privateMethod.invoke(urlService, "MD5");
+		});
+	}
+
+	@Test
+	void itShouldThrowApiNoSuchAlgorithmException() throws ReflectiveOperationException {
+		// given
+		UrlService urlService = new UrlService(urlRepository, urlMapper);
+		String algorithm = "AnyAlgorithmThatDoesntExist";
+
+		// when
+		TestMethod<UrlService, String> method =
+				TestMethod.method(UrlService.class, "getMessageDigest", String.class);
+
+		// then
+		assertThrows(ApiNoSuchAlgorithmException.class, () -> method.invoke(urlService, algorithm));
+	}
 }
